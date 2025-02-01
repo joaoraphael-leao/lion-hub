1ec88b20a2edab18511549746f1ef5d798f71f3e
joaorleao <jrlgo@ic.ufal.br>
2025-02-01 15:46:25 -0300

    Em desenvolvimento

--- README.md
+++ README.md
@@ -1,10 +1,52 @@
 # proj-software
 
-funcionalidades feitas: 1 e 2
-criar conta
-editar dados de conta
-deletar conta
+• User Account Management: Users can create, edit, and deactivate their accounts;
+- [x] Post Creation and Management: Users can create, edit, and delete posts, including text, images, and videos;
+ainda nao tem imagens e videos
+- [x] Friend and Follower System: Managing friend requests and follower connections;
+- [ ] Private Messaging: Enabling users to send private messages to one another
+- [ ] Group Creation and Management: Users can create and manage groups for shared interests;
+- [ ] Event Planning and Invitations: Organizing events and sending out invitations to contacts;
+- [ ] Content Filtering and Moderation: Implementing content filtering and moderation tools to maintain community standards;
+- [x] Notification System: Sending notifications about activities like comments, likes, and friend requests;
+ nots, ainda nao tem nots de likes
+- [ ] Privacy Settings: Allowing users to control the privacy of their profiles and posts;
+- [ ] User Analytics and Insights: Providing users with insights on their posts' reach and engagement.
 
-criar post com autor
-editar post 
-deletar post
+
+## Funcionality 1 
+- Create Accounts ✅
+- Edit Accounts
+- Deactivate Accounts 
+- Delete Accounts ✅
+
+## Funcionality 2
+
+- Create Posts ✅
+- Edit Posts
+- Delete posts
+- Put text ✅
+- Put images
+- Put videos
+ 
+## Funcionality 3 
+- Send messages ✅
+## Funcionality 4 
+- Create Groups
+## Funcionality 5 
+- Create events
+- Invite people
+## Funcionality 6 
+- Search for posts with filter 
+## Funcionality 7 
+- Notification for friend request ✅
+- Notification for message 
+- Notification for likes
+- Notification for comments
+## Funcionality 8 
+- Create a private profile
+- Accept follow requests
+## Funcionality 9 
+- Notifications for likes & comments.
+
+;
--- main.py
+++ main.py
@@ -1,36 +1,89 @@
 users = []
 postsList = []
 
+## coisas a implementar, lista de pessoas que curtiram
 class Post:
     def __init__(self, title, content, author):
         self._title = title
         self._content = content
-        self.author = author
+        self._author = author
+        self._likes = 0
+        self._coments = []
         self.id = len(postsList) + 1
         self._image = None
 
-    def setName(self, title):
+
+    def get_author(self):
+        return self._author
+
+    def getTitle(self):
+        return self._title
+
+    def set_title(self, title):
         self._title = title
+
+    def getContent(self):
+        return self._content
 
     def setContent(self, content):
         self._content = content
 
+    def getImage(self):
+        return self._image
+
     def setImage(self, image_url):
         self._image = image_url
 
+    def _update_likes(self):
+        self._likes += 1
+    def getLikes(self):
+        return self._likes
+
     def __str__(self):
-        return f'{self._title}: {self._content}\nImagem: {self._image if self._image else "Sem imagem"}'
+        return f"{self.get_author().getName()} Postou:\n{self._title}: {self._content}\nLikes: {self.getLikes()}"
+
+
 
 class User:
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, privacity):
         self._name = name
         self._email = email
         self._password = password
         self._id = len(users) + 1
         self.posts = []
+        self._notifications = []
+        self._followingList = []
+        self._followersList = []
+        self._privacity = privacity
+
+    def add_notification(self, notification):
+        self._notifications.append(notification)
+    def getName(self):
+        return self._name
+
+    def setName(self, name):
+        self._name = name
+
+    def getEmail(self):
+        return self._email
+
+    def setEmail(self, email):
+        self._email = email
+
+    def getPassword(self):
+        return self._password
+
+    def setPassword(self, password):
+        self._password = password
+
+    def getId(self):
+        return self._id
+
+    def getFollowingList(self):
+        return self._followingList
 
     def __str__(self):
-        return f'{self._name} ({self._email})'
+        return f"""   {self._name}\n   {self._email}\n"""
 
     def _generalEdit(self):
         option = input("""Opções:
@@ -40,13 +93,13 @@ class User:
         """)
 
         if option == '1':
-            self._name = input("Digite o novo nome: ")
+            self.setName(input("Digite o novo nome: "))
             print("Nome alterado com sucesso!")
         elif option == '2':
-            self._email = input("Digite o novo email: ")
+            self.setEmail(input("Digite o novo email: "))
             print("Email alterado com sucesso!")
         elif option == '3':
-            self._password = input("Digite a nova senha: ")
+            self.setPassword(input("Digite a nova senha: "))
             print("Senha alterada com sucesso!")
         else:
             print("Opção inválida!")
@@ -66,7 +119,7 @@ class User:
             post.setImage(image)
         postsList.append(post)
         self.posts.append(post)
-        print(f"Post criado com sucesso: {post._title}")
+        print(f"Post criado com sucesso: {post.getTitle()}")
 
     def deletePost(self):
         postId = input("Digite o ID do post a ser excluído: ")
@@ -78,11 +131,46 @@ class User:
                 return
         print("Você não tem permissão para deletar este post")
 
+    def follow_someone(self):
+        print("Escolha alguém para seguir")
+        showUsers(myUser=self)
+        followed_profile_name = input()
+        for user in users:
+            if followed_profile_name == user.getName():
+                self._followingList.append(user.getName())
+                user._notifications.append({
+                    "user": self.getName(),
+                   "message" : f"{self.getName()} começou a te seguir",
+                    "is_follow_notification": True,
+                })
+
+    def show_notifications(self):
+        for notification in self._notifications:
+            if(notification["is_follow_notification"]):
+                follow_back = input(notification["message"] + ", deseja seguí-lo de volta ? (y/n)")
+                if follow_back == "y":
+                    self._followingList.append(notification["user"])
+                    notification["user"]._notifications.append({
+                        "message": f"{self.getName()} te seguiu de volta!",
+                    })
+            else:
+                print(notification["message"])
+
+    def like_post(self, post_to_like):
+        post_to_like._update_likes
+        post_author = post_to_like.getAuthor()
+        notification = {
+            "message": f"{self.getName()} curtiu sua foto"
+        }
+        post_author.add_notification(notification)
+
+
 def seeAllPosts():
     if not postsList:
         print("Nenhum post disponível.")
     for post in postsList:
         print(post)
+
 
 def createPassword():
     while True:
@@ -92,13 +180,29 @@ def createPassword():
             return password
         print("Senhas não conferem! Tente novamente.")
 
-def add_user():
+
+def add_new_user():
     name = input("Diga seu nome: ")
     email = input("Diga seu email: ")
     password = createPassword()
-    user = User(name, email, password)  
-    users.append(user)  
+
+    privacity = False
+    privacity_option = input("Seu perfil será privado? (y/n)")
+    if privacity_option == 'y':
+        privacity = True
+
+    user = User(name, email, password, privacity= privacity)
+    users.append(user)
     print(f"Usuário {name} criado com sucesso!")
+
+
+def showUsers(myUser):
+    for i, user in enumerate(users):
+        if user == myUser:
+            continue
+        print(f"""Usuario {i + 1}: 
+    {user.getName()}""")
+
 
 def startApp():
     while True:
@@ -113,18 +217,12 @@ def startApp():
         Escolha uma opção: """)
 
         if opcao == '1':
-            add_user()
+            add_new_user()
         elif opcao == '2':
             seeAllPosts()
         elif opcao == '3':
             userEmail = input("Digite seu email: ")
-            myUser = None
-            for user in users:
-                if user._email == userEmail:
-                    passwordAttempt = input("Digite sua senha: ")
-                    if passwordAttempt == user._password:
-                        myUser = user
-                        break
+            myUser = next((user for user in users if user.getEmail() == userEmail), None)
 
             if not myUser:
                 print("Email ou senha incorretos!")
@@ -136,7 +234,9 @@ def startApp():
             2) Deletar Post
             3) Editar dados da conta
             4) Deletar Conta
-
+            5) Ver usuários
+            6) Exibir notificações
+            7) Seguir novas pessoas
             Escolha uma opção: """)
 
             if userOption == '1':
@@ -147,6 +247,12 @@ def startApp():
                 myUser._generalEdit()
             elif userOption == '4':
                 myUser._removeAccount()
+            elif userOption == '5':
+                showUsers()
+            elif userOption == '6':
+               myUser.show_notifications()
+            elif userOption == '7':
+                myUser.follow_someone()
             else:
                 print("Opção inválida!")
         elif opcao == '4':
@@ -154,5 +260,6 @@ def startApp():
             break
         else:
             print("Opção inválida!")
+
 
 startApp()
